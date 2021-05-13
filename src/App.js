4c8b56250049973351c129f34fa38e04eeb532f6
Anks1856 <ankur.chovatiya1856@gmail.com>
2021-05-13 17:29:50 +0530

    initial pages setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css"
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Home></Home>
+      <Rooms></Rooms>
+      <SingleRoom></SingleRoom>
+      <Error></Error>
     </div>
   );
 }
---
+++ src/pages/Rooms.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Rooms() {
+    return (
+        <div>
+            <h1>Hello from Rooms pages</h1>
+        </div>
+    )
+}
