4cc674535152b52df1a217fed880fb546360d841
Anks1856 <ankur.chovatiya1856@gmail.com>
2021-05-13 23:02:37 +0530

    services components added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Banner from '../components/Banner'
 import Hero from '../components/Hero'
+import Services from '../components/Services'
 
 function Home() {
     return (
+    <>
           <Hero>
               <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
                     <Link to="/rooms" className="btn-primary">Our rooms</Link>
               </Banner>
           </Hero>
+          <Services></Services>
+    </>
     )
 }
 
