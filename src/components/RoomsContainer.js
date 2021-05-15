54f0c40b60d852011a0019a74564f41c7414cc17
Anks1856 <ankur.chovatiya1856@gmail.com>
2021-05-15 17:44:20 +0530

    remove some warning

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import RoomsFilter from './RoomsFilter'
 import RoomsList from './RoomsList'
-import { RoomConsumer } from '../context'
+// import { RoomConsumer } from '../context'
 import Loading from './Loading'
 import { withRoomConsumer } from '../context'
 
--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import items from './data'
+// import items from './data'
 import Client from'./Contentful'
 // .then(response => console.log(response.items));
 // console.log(items);
